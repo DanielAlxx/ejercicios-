8a1d37eb0fe3f7910cc66348c6754ce22a62c9c2
Daniel Hernandez <daniel@extjs.mx>
2016-09-04 12:13:35 -0500

    cuadrado relleno

---
+++ ciclos/cuadrado_relleno.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+	
+	int n;
+
+	scanf("%d", &n);
+
+	for( int i = 0 ; i < n ; i++  ){
+		for( int j = 0 ; j < n ; j++ ){
+			
+			if( j == n-1 )
+				printf("*\n");
+			else if( i == 0 || i == n-1 || j == 0)
+				printf("*");
+			else
+				printf(" ");
+		}
+	}
+	
+
+	printf("\n");	
+
+	return 0;
+}
